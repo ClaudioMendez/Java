30a127df0b4fe725ca0a98f44b7c7917731f2198
ClaudioMendez <48967069+ClaudioMendez@users.noreply.github.com>
2020-03-06 09:11:24 -0300

    Mi Nombre

---
+++ MiNombre.java
@@ -0,0 +1,9 @@
+public class MiNombre{
+
+	public static void main(String[] args){
+
+		System.out.println("Mi nombre es Claudio Méndez");
+		System.out.println("Tengo 51 años de edad.");
+		System.out.println("Mi ciudad es Temuco, Chile");
+	}
+}
